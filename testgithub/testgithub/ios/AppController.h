719e996785ac63ab57c129ff17ee3bb3d38d0a1d
dodong471520 <1012131075@qq.com>
2013-01-18 16:02:51 +0800

    1

---
+++ testgithub/testgithub/ios/AppController.h
@@ -0,0 +1,20 @@
+//
+//  testgithubAppController.h
+//  testgithub
+//
+//  Created by yc on 13-1-18.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+@class RootViewController;
+
+@interface AppController : NSObject <UIAccelerometerDelegate, UIAlertViewDelegate, UITextFieldDelegate,UIApplicationDelegate> {
+    UIWindow *window;
+    RootViewController    *viewController;
+}
+
+@property (nonatomic, retain) UIWindow *window;
+@property (nonatomic, retain) RootViewController *viewController;
+
+@end
+
---
+++ testgithub/testgithub/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  testgithubAppController.h
+//  testgithub
+//
+//  Created by yc on 13-1-18.
+//  Copyright __MyCompanyName__ 2013年. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
